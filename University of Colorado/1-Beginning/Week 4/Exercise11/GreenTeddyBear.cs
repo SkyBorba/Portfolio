5d6656f3a6c831140f9cba922b7608660080e73a
SkyBorba <skyborba13@gmail.com>
2025-04-15 16:08:04 +0100

    Add files via upload

---
+++ University of Colorado/1-Beginning/Week 4/Exercise11/GreenTeddyBear.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenTeddyBear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // triple height
+        Vector3 newScale = transform.localScale;
+
+        newScale.y = newScale.y * 3;
+
+        transform.localScale = newScale;
+    }
+}
---
+++ University of Colorado/1-Beginning/Week 4/Exercise13/TeddyBear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A teddy bear
+/// </summary>
+public class TeddyBear : MonoBehaviour
+{
+	/// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+	void Start()
+    {
+        // get the teddy bear moving
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+    }
+	
+}
